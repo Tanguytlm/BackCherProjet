0fcb91f07a455a0de9a32fbee0e79b1c842bbfba
Tanguytlm <tanguy0789@gmail.com>
2020-03-05 15:30:35 +0100

    checking mdp

--- BackCherProjet/src/main/java/fr/solutec/dao/UtilisateurRepository.java
+++ BackCherProjet/src/main/java/fr/solutec/dao/UtilisateurRepository.java
@@ -1,5 +1,8 @@
 package fr.solutec.dao;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import fr.solutec.entities.Utilisateur;
 
-public interface UtilisateurRepository {
-
+public interface UtilisateurRepository extends CrudRepository<Utilisateur, Long>{
+	public Optional<Utilisateur> findByMailAndMdp(String mail,String mdp);
 }
--- BackCherProjet/src/main/java/fr/solutec/rest/UtilisateurRest.java
+++ BackCherProjet/src/main/java/fr/solutec/rest/UtilisateurRest.java
@@ -1,6 +1,31 @@
 package fr.solutec.rest;
 
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.solutec.dao.UtilisateurRepository;
+import fr.solutec.entities.Utilisateur;
+
+@ RestController @CrossOrigin("*")
 public class UtilisateurRest {
+	@Autowired
+	private UtilisateurRepository utilisateurRepo;
+	
+	@RequestMapping(value = "/person", method = RequestMethod.POST)
+	public Utilisateur getConnexion(@RequestBody Utilisateur p) {
+		Optional<Utilisateur> utilisateur = utilisateurRepo.findByMailAndMdp(p.getMail(), p.getMdp());
+		Utilisateur pe = new Utilisateur();
+		if(utilisateur.isPresent()) {
+			pe = utilisateur.get();
+			pe.setMdp("");
+		}
+		return pe;
+	}
 	
 }
