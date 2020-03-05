268e969eae61feb2c690cbef76c87147821d6c88
Tanguytlm <tanguy0789@gmail.com>
2020-03-05 15:48:34 +0100

    on fait venir du monde

--- BackCherProjet/src/main/java/fr/solutec/BackCherProjetApplication.java
+++ BackCherProjet/src/main/java/fr/solutec/BackCherProjetApplication.java
@@ -1,12 +1,24 @@
 package fr.solutec;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.solutec.dao.EntrepriseRepository;
+import fr.solutec.dao.ParticulierRepository;
+import fr.solutec.dao.UtilisateurRepository;
+import fr.solutec.entities.Entreprise;
+import fr.solutec.entities.Particulier;
+
 @SpringBootApplication
 public class BackCherProjetApplication implements CommandLineRunner{
 
+	@Autowired
+	private ParticulierRepository particulierRepo;
+	private EntrepriseRepository entrepriseRepo;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackCherProjetApplication.class, args);
 		System.out.println("finishhhhhhhhhhhhhhhhhhhhhhhhhh");
@@ -15,6 +27,9 @@ public class BackCherProjetApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("okkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+		Particulier p1= new Particulier("monMail@serveur.com","monMotDePasse",1,"monNom","monPrenom",15,null,null,null);
+		Entreprise e1= new Entreprise("mailEntreprise","motDePasseEntreprise",0,"nomEntreprise","la crêpe","adresseEntreprise",75000,"0652145654",null,"maDescription");
+		
 		
 	}
 
