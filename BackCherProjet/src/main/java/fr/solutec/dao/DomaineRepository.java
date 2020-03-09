1ca42a60d16d4a447ccc579e20b458d4056de6fa
Tanguytlm <tanguy0789@gmail.com>
2020-03-09 14:16:29 +0100

    update

--- BackCherProjet/src/main/java/fr/solutec/dao/DomaineRepository.java
+++ BackCherProjet/src/main/java/fr/solutec/dao/DomaineRepository.java
@@ -1,9 +1,11 @@
 package fr.solutec.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Domaine;
 
 public interface DomaineRepository extends CrudRepository<Domaine, Long> {
-
+	public List<Domaine> findByIdDomaine(Long id);
 }
--- BackCherProjet/src/main/java/fr/solutec/rest/DomaineRest.java
+++ BackCherProjet/src/main/java/fr/solutec/rest/DomaineRest.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.solutec.dao.DomaineRepository;
 import fr.solutec.entities.Domaine;
+import fr.solutec.entities.Participation;
 
 @RestController @CrossOrigin("*")
 public class DomaineRest {
@@ -22,4 +24,8 @@ public class DomaineRest {
 		return (List<Domaine>) domaineRepo.findAll();
 		
 	}
+	
+	@RequestMapping(value = "/domaine/{id}", method = RequestMethod.GET)
+	public List<Domaine> getDomaine(@PathVariable Long id){
+		return  (List<Domaine>) domaineRepo.findByIdDomaine(id);}
 }
