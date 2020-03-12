c26f40303285aadc8be1653ba70678ba0faa97a3
Tanguytlm <tanguy0789@gmail.com>
2020-03-12 15:54:33 +0100

    tada

--- BackCherProjet/src/main/java/fr/solutec/dao/DomaineProjetRepository.java
+++ BackCherProjet/src/main/java/fr/solutec/dao/DomaineProjetRepository.java
@@ -9,6 +9,6 @@ import fr.solutec.entities.DomaineEntreprise;
 import fr.solutec.entities.DomaineProjet;
 
 public interface DomaineProjetRepository extends CrudRepository<DomaineProjet, Long> {
-	public Optional<DomaineProjet> findByProjetIdProjet(Long id);
+	public List<DomaineProjet> findByProjetIdProjet(Long id);
 	public List<DomaineProjet> findByDomaineIdDomaine(Long id);
 }
